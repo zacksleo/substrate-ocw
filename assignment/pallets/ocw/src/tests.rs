c437e85d189aadce515c08969d6ed40dd7dcf08e
zacksleo <zacksleo@gmail.com>
2021-09-11 00:15:09 +0800

    initial commit

---
+++ assignment/pallets/ocw/src/tests.rs
@@ -0,0 +1,13 @@
+use sp_runtime::Permill;
+
+use crate::PolkadotPrice;
+
+#[test]
+pub fn permill_test() {
+	let s = "30.8287640368510822";
+	let price_usd: Vec<&str> = s.split(".").collect();
+	let price_usd_num: u64 = price_usd[0].parse().unwrap();
+	let price_usd_permill: Permill = Permill::from_parts(price_usd[1][..6].parse::<u32>().unwrap());
+	assert_eq!(price_usd_num, 30);
+	assert_eq!(price_usd_permill, Permill::from_parts(828764))
+}
